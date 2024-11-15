c783b83f1807449d1130c2ebd958bde6b77154e3
mohammadalnseirat <146799067+mohammadalnseirat@users.noreply.github.com>
2024-11-15 21:37:32 +0400

    Create Get TV Trailers Api Route

--- backend/controllers/tv.controller.js
+++ backend/controllers/tv.controller.js
@@ -14,3 +14,17 @@ export const getTrendingTV = async (req, res, next) => {
     next(error);
   }
 };
+
+//! 2-Function To Get Trailers Tv Series:
+export const getTvTrailers = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const data = await fetchFromTMDB(
+      `https://api.themoviedb.org/3/tv/${id}/videos?language=en-US`
+    );
+    res.status(200).json({ trailers: data.results });
+  } catch (error) {
+    console.log("Error getting trending TV series", error.message);
+    next(error);
+  }
+};
--- backend/routes/tv.route.js
+++ backend/routes/tv.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getTrendingTV } from "../controllers/tv.controller.js";
+import { getTrendingTV, getTvTrailers } from "../controllers/tv.controller.js";
 
 const router = express.Router();
 
 router.get("/trending", getTrendingTV);
+router.get('/:id/trailers',getTvTrailers);
 
 export default router;
